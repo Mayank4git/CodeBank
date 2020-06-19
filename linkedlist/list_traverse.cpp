3d0445e46aeed2f427703f2abd38a92718ae5c2e
Mayank kaushik <mayankvip9@gmail.com>
2020-06-19 18:03:20 +0530

    LInklist in C++

---
+++ linkedlist/list_traverse.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+class linked_list
+{
+private:
+    node *head,*tail;
+public:
+    linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void add_node(int n)
+    {
+        node *tmp = new node;
+        tmp->data = n;
+        tmp->next = NULL;
+
+        if(head == NULL)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else
+        {
+            tail->next = tmp;
+            tail = tail->next;
+        }
+    }
+
+    void display()
+    {
+        node *tmp;
+        tmp = head;
+        while (tmp != NULL)
+        {
+            cout << tmp->data << endl;
+            tmp = tmp->next;
+        }
+    }
+};
+
+int main()
+{
+    linked_list a;
+    a.add_node(1);
+    a.add_node(2);
+    a.display();
+    return 0;
+}
