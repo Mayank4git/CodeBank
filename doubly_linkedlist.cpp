0e4eacd116126fcdd69076a97af701cbdb5a94af
Mayank kaushik <mayankvip9@gmail.com>
2020-06-26 17:59:18 +0530

    Update doubly_linkedlist.cpp

--- doubly_linkedlist.cpp
+++ doubly_linkedlist.cpp
@@ -1,30 +1,24 @@
 #include<iostream>
 using namespace std;
-
 struct node
 {
 	int data;
 	node *prev;
 	node *next;
-
 };
 class doublylist
 {
 private:
 	node *head,*tail;
-
 public:
 	doublylist()
 	{
 		head=NULL;
 		tail=NULL;
 	}
-
   void addtohead(int value)
   {
-   
-  node *newNode=new node;
-   
+    node *newNode=new node;
     newNode -> data = value;
     newNode -> prev = NULL;
     if(head == NULL)
@@ -38,13 +32,9 @@ public:
        head = newNode;
     }
     cout<<"\nInsertion successful at the head"<<endl;
-
-  }
-
-  void insertAtEnd(int v)
-{
-    node *newNode=new node;
-  
+ }
+void insertAtEnd(int v)
+{   node *newNode=new node;
    newNode -> data = v;
    newNode -> next = NULL;
    if(head == NULL)
@@ -53,17 +43,14 @@ public:
       head = newNode;
    }
    else
-   {
-       node *temp = head;
+   {  node *temp = head;
       while(temp -> next != NULL)
-         temp = temp -> next;
+      temp = temp -> next;
       temp -> next = newNode;
       newNode -> prev = temp;
    }
    cout<<"\nInsertion success!!! at the end"<<endl;   
 }
-
-
 void deleteBeginning()
 {
    if(head == NULL)
@@ -84,8 +71,6 @@ void deleteBeginning()
       cout<<"Deletion success at the front"<<endl;
    }
 }
-
-
 void deleteEnd()
 {
    if(head == NULL)
@@ -107,12 +92,8 @@ void deleteEnd()
       cout<<"Deletion successful at the end "<<endl;
    }
 }
-
-
 void insert_specified(int item)  
-{  
-     
-    node *ptr = new node;  
+{  node *ptr = new node;  
    node *temp;   
    int loc;   
    if(ptr == NULL)  
@@ -136,7 +117,6 @@ void insert_specified(int item)
        cout<<"Node Inserted at the desired position \n";  
    }  
 }
-
 void search_element(int data)
 {
      node * temp = new node;
@@ -165,13 +145,8 @@ void search_element(int data)
     else
     {
     cout<<"\nThe element  is not found in the list";
-    }
-
-   
+    } 
 }
-
-
-
 void deleteFromN(int position)
 {
     node *current;
@@ -180,7 +155,6 @@ void deleteFromN(int position)
     {
         current = current->next;
     }
-
     if(position == 0)
     {
        deleteBeginning();
@@ -189,20 +163,31 @@ void deleteFromN(int position)
     {
         current->prev->next = current->next;
         current->next->prev = current->prev;
-
         delete current; 
-
         cout<<"SUCCESSFULLY DELETED NODE FROM POSITION.\n";
     }
     else
     {
-        printf("Invalid position!\n");
+        cout<<"Invalid position!\n";
     }
 }
-
+void reverse()
+{
+    node *current = head;
+    node *temp = NULL;
+    while(current!=NULL)
+    {
+      temp = current->prev;   //swap the next and prev pointer
+      current->prev = current->next;
+      current->next = temp;
+      current = current->prev;
+    }
+    temp->prev;
+    cout<<"LIST REVERSED SUCCESSFULLY.\n";
+}
  void display()  
 {  
-    struct node *ptr;  
+    node *ptr;  
     cout<<"printing values"<<endl;  
     ptr = head;  
     while(ptr != NULL)  
@@ -211,28 +196,22 @@ void deleteFromN(int position)
         ptr=ptr->next;  
     }  
 }  
-
-
 };
-
-
-
 int main()
-{
-  doublylist d;
+{doublylist d;
  while(1)
- {
- 	int n;
+ {      int n;
  	cout<<endl;
  	cout<<"enter you choice"<<endl;
  	cout<<"1. add to head"<<endl;
  	cout<<"2. add to end"<<endl;
  	cout<<"3. delete from head"<<endl;
-  cout<<"4. delete from end"<<endl;
+        cout<<"4. delete from end"<<endl;
  	cout<<"5. display"<<endl;
  	cout<<"6. insert  at desired position"<<endl;
-  cout<<"7. search  in doubly linked list"<<endl;
-  cout<<"8. delete from desired position"<<endl;
+        cout<<"7. search  in doubly linked list"<<endl;
+        cout<<"8. delete from desired position"<<endl;
+        cout<<"9. reverse"<<endl;
     cin>>n;
     switch(n)
     {
@@ -242,55 +221,42 @@ int main()
     	cin>>m;
     	d.addtohead(m);
     	break;
-        
-
     	case 2:
     	int item;
     	cout<<"enter item to add to head"<<endl;
     	cin>>item;
     	d.insertAtEnd(item);
-    	break;
-       
-      case 3:
+    	break; 
+        case 3:
        d.deleteBeginning();
        break;
-      case 4:
+       case 4:
        d.deleteEnd();
        break;
-
-
-    	case 5:
-    	d.display();
-    	break;
-
-    	case 6:
+       case 5:
+       d.display();
+       break;
+       case 6:
       int newvalue;
       cout<<"enter value for desired position\n";
       cin>>newvalue;
-    	d.insert_specified(newvalue);
-    	break;
-
+      d.insert_specified(newvalue);
+      break;
       case 7:
       int newvalue1;
       cout<<"enter value for searching position\n";
       cin>>newvalue1;
       d.search_element(newvalue1);
       break;
-
       case 8:
       int newvalue2;
       cout<<"enter position desired for deletion \n";
       cin>>newvalue2;
       d.deleteFromN(newvalue2);
       break;
-
-    	default:
-    	cout<<"exit successfull";
-
-
-    }
-
-
- }
-
+      case 9:
+      d.reverse();
+      break;
+      default:
+      cout<<"exit successfull";}}
 }
