926395414b4d6bb46e587c52d2bf07c1c0775181
Niteshyadav585 <36307599+Niteshyadav585@users.noreply.github.com>
2020-10-03 13:16:53 +0530

    Update Bubblesort.cpp

--- sorting/Bubblesort.cpp
+++ sorting/Bubblesort.cpp
@@ -1,28 +1,27 @@
 #include<iostream>  
 using namespace std;
-int main ()
-{
-   int n,a[n],temp;
+int main (){
+   int n; 
+   int a[n];
    cout<<"enter size";
    cin>>n;
+   
    cout <<"Input elements \n";
    for(int i = 0; i<n; i++) {
       cin>>a[i];
     }
 
-for(int i = 0; i<n; i++) {
-   for(int j = 0; j<n-i-1; j++)
-   {
-      if(a[j+1] < a[j]) {
-         temp = a[j];
-         a[j] = a[j+1];
-         a[j+1] = temp;
+   for(int i = 0; i<n; i++) {
+      for(int j = 0; j<n-i-1; j++){
+         if(a[j+1] < a[j]) {
+            swap( a[j], a[j+1] );
+         }
       }
    }
-}
-cout <<"Sorted Element List ...\n";
-for(int i = 0; i<n; i++) {
-   cout <<a[i]<<"\t";
-   }
-return 0;
+   
+   cout <<"Sorted Element List ...\n";
+   for(int i = 0; i<n; i++) {
+      cout <<a[i]<<"\t";
+      }
+   return 0;
 }
