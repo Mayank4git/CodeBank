b06077562c40577a2be8b6b7a8af5fa35bad3b89
niteshY <72301137+niteshpg@users.noreply.github.com>
2020-10-03 16:19:53 +0530

    Update diagonalsum.cpp

--- array_examples/diagonalsum.cpp
+++ array_examples/diagonalsum.cpp
@@ -6,24 +6,15 @@ using namespace std;
  	cout<<"enter size of matrix";
  	cin>>n;
  	cout<<"enter matrix elements";
- 	for(int i=0;i<n;i++)
- 	{
- 		for(int j=0;j<n;j++)
- 		{
+ 	for(int i=0;i<n;i++){
+ 		for(int j=0;j<n;j++){
  			cin>>a[i][j];
  		}
  	}
  
-    int sum=0;
-	for(int i=0;i<n;i++)
- 	{
- 		for(int j=0;j<n;j++)
- 		{
- 			if(i=j)
- 			{
- 				sum=sum+a[i][j];
- 			}
- 		}
+        int sum=0;
+ 	for(int j=0;j<n;j++){
+ 	    sum=sum+a[j][j];
  	}
  	
  	 cout<<"sum of diagonal elements"<<sum;
